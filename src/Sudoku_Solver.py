98e2a29acaaaeaa80ff6fc419561e243d63dfe0d
Rudra Patel <patelrudra2003@gmail.com>
2024-04-10 09:02:07 -0400

    Run autopep8 formatter

--- src/Sudoku_Solver.py
+++ src/Sudoku_Solver.py
@@ -3,26 +3,26 @@ The solution is implemented using recursive backtracking
 """
 import copy
 
-#type in a game of sudoku here
+# type in a game of sudoku here
 sudoku_board = [
-        [5,3,0,   0,7,0,   0,0,0],
-        [6,0,0,   1,9,5,   0,0,0],
-        [0,9,8,   0,0,0,   0,6,0],
+    [5, 3, 0,   0, 7, 0,   0, 0, 0],
+    [6, 0, 0,   1, 9, 5,   0, 0, 0],
+    [0, 9, 8,   0, 0, 0,   0, 6, 0],
 
-        [8,0,0,   0,6,0,   0,0,3],
-        [4,0,0,   8,0,3,   0,0,1],
-        [7,0,0,   0,2,0,   0,0,6],
-        
-        [0,6,0,   0,0,0,   2,8,0],
-        [0,0,0,   4,1,9,   0,0,5],
-        [0,0,0,   0,8,0,   0,7,9]
+    [8, 0, 0,   0, 6, 0,   0, 0, 3],
+    [4, 0, 0,   8, 0, 3,   0, 0, 1],
+    [7, 0, 0,   0, 2, 0,   0, 0, 6],
+
+    [0, 6, 0,   0, 0, 0,   2, 8, 0],
+    [0, 0, 0,   4, 1, 9,   0, 0, 5],
+    [0, 0, 0,   0, 8, 0,   0, 7, 9]
 ]
 
 
 def main() -> None:
-    #create a copy of the original board before it mutates
+    # create a copy of the original board before it mutates
     original_board = copy.deepcopy(sudoku_board)
-    
+
     print(f"The original board is:\n{create_ASCII_board(original_board)}")
 
     if solve_sudoku(sudoku_board):
@@ -33,29 +33,29 @@ def main() -> None:
 
 def solve_sudoku(board: list[list[int]]) -> bool:
     """Takes in a 9x9 sudoku board and returns True if it has modified it to a solution and False if a solution does not exist"""
-    #find the first empty cell
+    # find the first empty cell
     row, column = find_next_empty_cell(board)
 
-    #check if the sudoku is solved
+    # check if the sudoku is solved
     if row == -1 and column == -1:
-        return True 
-    
-    #input a guess into the empty cell
-    for guess in range (1,10):
-        #check if the guess is valid
+        return True
+
+    # input a guess into the empty cell
+    for guess in range(1, 10):
+        # check if the guess is valid
         if is_valid_guess(board, guess, row, column):
-            #if the guess is valid, insert the guess into the board
+            # if the guess is valid, insert the guess into the board
             board[row][column] = guess
 
-            #recursively call the function again
+            # recursively call the function again
             if solve_sudoku(board):
                 return True
 
-        #if this stage is reached, the guess is not valid or the previous guesses did not result in a valid solution
-        #in this case, reset the guess
+        # if this stage is reached, the guess is not valid or the previous guesses did not result in a valid solution
+        # in this case, reset the guess
         board[row][column] = 0
-    
-    #if this step is reached, the input board is unsolvable
+
+    # if this step is reached, the input board is unsolvable
     return False
 
 
@@ -65,28 +65,28 @@ def find_next_empty_cell(board: list[list[int]]) -> tuple[int, int]:
         for column in range(9):
             if board[row][column] == 0:
                 return (row, column)
-            
+
     return (-1, -1)
 
 
 def is_valid_guess(board: list[list[int]], guess: int, row: int, column: int) -> bool:
     """given an input guess, returns True if it is valid through the rules of Sudoku and False otherwise"""
-    #check 1: the same int can not appear in the same row
+    # check 1: the same int can not appear in the same row
     for i in range(9):
         if board[row][i] == guess:
             return False
 
-    #check 2: the same int can not appear in the same column
+    # check 2: the same int can not appear in the same column
     for i in range(9):
         if board[i][column] == guess:
             return False
-    
-    #check 3: the same int can not appear in the same box
-    #first find which box the guess is in 
+
+    # check 3: the same int can not appear in the same box
+    # first find which box the guess is in
     box_row = row // 3
     box_column = column // 3
 
-    #knowing what box the guess is in, interate through that box
+    # knowing what box the guess is in, interate through that box
     starting_row = box_row * 3
     starting_column = box_column * 3
 
@@ -94,29 +94,29 @@ def is_valid_guess(board: list[list[int]], guess: int, row: int, column: int) ->
         for j in range(3):
             if board[starting_row + i][starting_column + j] == guess:
                 return False
-    
-    #if all other checks pass, the guess is valid
+
+    # if all other checks pass, the guess is valid
     return True
 
 
 def create_ASCII_board(board: list[list[int]]) -> str:
     """converts the board into an ASCII representation for clarity"""
     output_string = ""
-    
+
     for i in range(9):
-        #print a horizontal line for every 3 rows, but not before the first 3 rows
+        # print a horizontal line for every 3 rows, but not before the first 3 rows
         if i % 3 == 0 and i != 0:
             output_string += "- - - - - - - - - - -\n"
 
         for j in range(9):
-            #print a vertical line after every 3 numbers, but not before the first 3 numbers
+            # print a vertical line after every 3 numbers, but not before the first 3 numbers
             if j % 3 == 0 and j != 0:
                 output_string += "| "
-            
-            #print numbers, going to a new line if the number is the last one in a row
+
+            # print numbers, going to a new line if the number is the last one in a row
             if j == 8:
                 output_string += f"{board[i][j]}\n"
-            else: 
+            else:
                 output_string += f"{board[i][j]} "
 
     return output_string
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 """This file presents an interactive sudoku game that a user can play
 If desired, the user can utilize the sudoku backtracking algorithm to solve the sudoku for them
 """
-from __future__ import annotations #used to typehint a class that isn't defined yet (see SudokuBoard.deselect_cell())
+from __future__ import annotations  # used to typehint a class that isn't defined yet (see SudokuBoard.deselect_cell())
 from Sudoku_Solver import is_valid_guess, find_next_empty_cell
 import pygame
 
-#constants
+# constants
 BACKGROUND_COLOR = (255, 255, 255)
 GRIDLINE_COLOR = (0, 0, 0)
 CELL_FONT_COLOR = (0, 0, 0)
@@ -27,12 +27,12 @@ FOOTER_FONT_SIZE = FOOTER_HEIGHT // 2
 
 FONT_FILE_PATH = "src/assets/fonts/CourierPrimeSans.ttf"
 
-ANIMATION_DELAY = 1 #millisecond
+ANIMATION_DELAY = 1  # millisecond
 
 
 class SudokuBoard:
     """This class manages the sudoku board state and draws it onto the window
-    
+
     Attributes:
         window: the pygame window
         cells (list[list[Cell]]): contains the 81 cells accesible by row and column
@@ -40,89 +40,91 @@ class SudokuBoard:
         currently_selected_cell (tuple(int, int)): the row and column index of the currently selected cell
         footer_message (str): the message that is to be displayed on the footer, based on the board state
     """
+
     def __init__(self, window: pygame.Surface) -> None:
         """Initializes the sudoku board and its cells"""
         self.window = window
         self.board = [
-        [5,3,0,   0,7,0,   0,0,0],
-        [6,0,0,   1,9,5,   0,0,0],
-        [0,9,8,   0,0,0,   0,6,0],
+            [5, 3, 0,   0, 7, 0,   0, 0, 0],
+            [6, 0, 0,   1, 9, 5,   0, 0, 0],
+            [0, 9, 8,   0, 0, 0,   0, 6, 0],
 
-        [8,0,0,   0,6,0,   0,0,3],
-        [4,0,0,   8,0,3,   0,0,1],
-        [7,0,0,   0,2,0,   0,0,6],
-        
-        [0,6,0,   0,0,0,   2,8,0],
-        [0,0,0,   4,1,9,   0,0,5],
-        [0,0,0,   0,8,0,   0,7,9]
+            [8, 0, 0,   0, 6, 0,   0, 0, 3],
+            [4, 0, 0,   8, 0, 3,   0, 0, 1],
+            [7, 0, 0,   0, 2, 0,   0, 0, 6],
+
+            [0, 6, 0,   0, 0, 0,   2, 8, 0],
+            [0, 0, 0,   4, 1, 9,   0, 0, 5],
+            [0, 0, 0,   0, 8, 0,   0, 7, 9]
         ]
-        self.cells = [[SudokuCell(self.board[i][j], i, j) for j in range(9)] for i in range(9)]
+        self.cells = [[SudokuCell(self.board[i][j], i, j)
+                       for j in range(9)] for i in range(9)]
         self.currently_selected_cell = None
         self.footer_message = None
 
-  
     def draw_board(self) -> None:
         """Draws the sudoku board with a background, gridlines, cells, and footer message"""
-        #draw background
+        # draw background
         self.window.fill(BACKGROUND_COLOR)
 
-        #draw horizontal gridlines
+        # draw horizontal gridlines
         for i in range(10):
             if i % 3 == 0:
-                pygame.draw.line(self.window, GRIDLINE_COLOR, (0, CELL_SIDE_LENGTH * i), (BOARD_WIDTH, CELL_SIDE_LENGTH * i), THICK_LINE_WIDTH)
+                pygame.draw.line(self.window, GRIDLINE_COLOR, (0, CELL_SIDE_LENGTH * i),
+                                 (BOARD_WIDTH, CELL_SIDE_LENGTH * i), THICK_LINE_WIDTH)
             else:
-                pygame.draw.line(self.window, GRIDLINE_COLOR, (0, CELL_SIDE_LENGTH * i), (BOARD_WIDTH, CELL_SIDE_LENGTH * i), THIN_LINE_WIDTH)
-        
-        #draw vertical gridlines
+                pygame.draw.line(self.window, GRIDLINE_COLOR, (0, CELL_SIDE_LENGTH * i),
+                                 (BOARD_WIDTH, CELL_SIDE_LENGTH * i), THIN_LINE_WIDTH)
+
+        # draw vertical gridlines
         for i in range(10):
             if i % 3 == 0:
-                pygame.draw.line(self.window, GRIDLINE_COLOR, (CELL_SIDE_LENGTH * i, 0), (CELL_SIDE_LENGTH * i, BOARD_HEIGHT), THICK_LINE_WIDTH)
+                pygame.draw.line(self.window, GRIDLINE_COLOR, (CELL_SIDE_LENGTH * i, 0),
+                                 (CELL_SIDE_LENGTH * i, BOARD_HEIGHT), THICK_LINE_WIDTH)
             else:
-                pygame.draw.line(self.window, GRIDLINE_COLOR, (CELL_SIDE_LENGTH * i, 0), (CELL_SIDE_LENGTH * i, BOARD_HEIGHT), THIN_LINE_WIDTH)
-        
-        #draw cells
+                pygame.draw.line(self.window, GRIDLINE_COLOR, (CELL_SIDE_LENGTH * i, 0),
+                                 (CELL_SIDE_LENGTH * i, BOARD_HEIGHT), THIN_LINE_WIDTH)
+
+        # draw cells
         for i in range(9):
             for j in range(9):
                 self.cells[i][j].draw_cell(self.window)
-        
-        #draw footer message
+
+        # draw footer message
         if self.footer_message is not None:
             self.display_footer_message(self.footer_message)
-    
-    
+
     def select_cell(self, mouse_x: int, mouse_y: int) -> None:
         """Selects a cell to interact with it
-        
+
         Arguments:
             mouse_x: the x position of the mouse on mouseclick
             mouse_y: the y position of the mouse on mouseclick
         """
-        #make every cell not selected
+        # make every cell not selected
         for i in range(9):
             for j in range(9):
                 self.cells[i][j].is_selected = False
-        
-        #only look at clicks on the board
+
+        # only look at clicks on the board
         if mouse_x > BOARD_WIDTH or mouse_y > BOARD_HEIGHT:
             return
 
-        #find the cell the coordinates are pointing to
+        # find the cell the coordinates are pointing to
         cell_x, cell_y = mouse_x // CELL_SIDE_LENGTH, mouse_y // CELL_SIDE_LENGTH
 
-        #set the correct cell to selected
+        # set the correct cell to selected
         cell = self.cells[cell_y][cell_x]
         cell.is_selected = True
 
-        #record the index of which cell is selected
+        # record the index of which cell is selected
         self.currently_selected_cell = (cell_y, cell_x)
-
 
     def deselect_cell(self, cell: SudokuCell) -> None:
         """Deselects a selected cell"""
         cell.is_selected = False
         self.currently_selected_cell = None
 
-    
     def display_guess(self, input_number: int) -> None:
         """Adds a temporary guess on a selected cell and then deselects it
         Arguments:
@@ -132,7 +134,6 @@ class SudokuBoard:
         cell = self.cells[row][column]
         if cell.is_editable:
             cell.guess = input_number
-    
 
     def clear_cell(self) -> None:
         """Clears a selected cell of placed values or temporary guesses and then deselects it"""
@@ -141,9 +142,8 @@ class SudokuBoard:
         if cell.is_editable:
             cell.value = 0
             cell.guess = 0
-            self.board[row][column] = 0 #update board state
+            self.board[row][column] = 0  # update board state
         self.deselect_cell(cell)
-
 
     def lock_in_guess(self) -> None:
         """"Locks in" the temporary guess and deselects the cell"""
@@ -158,40 +158,37 @@ class SudokuBoard:
             self.deselect_cell
             return
 
-        #turn guess into value 
+        # turn guess into value
         if is_valid_guess(self.board, cell.guess, row, column):
             cell.value = cell.guess
             cell.guess = 0
-            self.board[row][column] = cell.value #update board state
+            self.board[row][column] = cell.value  # update board state
             self.footer_message = None
             self.deselect_cell(cell)
-        
-        else: 
+
+        else:
             cell.guess = 0
             self.footer_message = "Invalid Guess"
             self.deselect_cell(cell)
-        
+
         if self.is_solved(self.board):
-            #remove the user's ability to edit the board
+            # remove the user's ability to edit the board
             self.freeze_board()
             self.footer_message = "Congratulations!"
-
 
     def is_solved(self, board: list[list[int]]) -> bool:
         """Returns True if the board is solved and False if not"""
         for i in range(9):
             for j in range(9):
                 if self.board[i][j] == 0:
-                  return False
+                    return False
         return True
-    
 
     def freeze_board(self) -> None:
         """Makes the board unable to be edited further"""
         for i in range(9):
-                for j in range(9):
-                    self.cells[i][j].is_editable = False
-
+            for j in range(9):
+                self.cells[i][j].is_editable = False
 
     def display_footer_message(self, message: str) -> None:
         """Displays an input message onto the footer"""
@@ -200,60 +197,60 @@ class SudokuBoard:
 
         footer_text_height = footer_text.get_height()
         footer_text_width = footer_text.get_width()
-    
-        self.window.blit(footer_text, (BOARD_WIDTH // 2 - footer_text_width // 2, BOARD_HEIGHT + FOOTER_HEIGHT // 2 - footer_text_height // 2))
-    
+
+        self.window.blit(footer_text, (BOARD_WIDTH // 2 - footer_text_width //
+                         2, BOARD_HEIGHT + FOOTER_HEIGHT // 2 - footer_text_height // 2))
 
     def auto_solve(self) -> bool:
         """Visualizes the sudoku backtracking algorithm as it solves the board for the user
-        
+
         Returns:
             bool: True if the input board is solvable and False otherwise
         """
-        #there is no need to freeze the board as function execute stops the pygame event loop from registering inputs
-        #prevent window from freezing (which occurs from pygame event loop stopping)
+        # there is no need to freeze the board as function execute stops the pygame event loop from registering inputs
+        # prevent window from freezing (which occurs from pygame event loop stopping)
         pygame.event.pump()
-        
-        #deselect cells
+
+        # deselect cells
         for i in range(9):
             for j in range(9):
                 self.cells[i][j].is_selected = False
 
-        #find first empty cell; if none, the board is solved
+        # find first empty cell; if none, the board is solved
         row, column = find_next_empty_cell(self.board)
 
         if row == -1 or column == -1:
             return True
 
-        #try guesses and recursively call function
-        #edit both the game window and the board state
-        for guess in range (1,10):
-            self.cells[row][column].is_selected = True #creates visual effect
+        # try guesses and recursively call function
+        # edit both the game window and the board state
+        for guess in range(1, 10):
+            self.cells[row][column].is_selected = True  # creates visual effect
 
             if is_valid_guess(self.board, guess, row, column):
                 self.cells[row][column].value = guess
                 self.board[row][column] = guess
 
-                #update the window
+                # update the window
                 self.draw_board()
                 pygame.display.update()
                 pygame.time.delay(ANIMATION_DELAY)
 
-                #recursion
+                # recursion
                 if self.auto_solve():
                     return True
 
-            #if a guess doesn't result in a solved board, reset the guess
+            # if a guess doesn't result in a solved board, reset the guess
             self.cells[row][column].value = 0
             self.board[row][column] = 0
 
-        #return False if the input board did not result in a solution
+        # return False if the input board did not result in a solution
         return False
-            
+
 
 class SudokuCell:
     """This class represents models the 81 cells of sudoku
-    
+
     Attributes:
         value: the number in the cell
         row: the row index of the cell
@@ -262,6 +259,7 @@ class SudokuCell:
         is_selected (bool): True if a cell is clicked on and False otherwise
         is_editable (bool): True if a cell can be edited and False otherwise
     """
+
     def __init__(self, value: int, row: int, column: int) -> None:
         """Initializes the cell"""
         self.value = value
@@ -270,44 +268,49 @@ class SudokuCell:
         self.guess = 0
         self.is_selected = False
 
-        #if a cell has a value upon creation its value is not changeable
+        # if a cell has a value upon creation its value is not changeable
         if self.value:
             self.is_editable = False
         else:
             self.is_editable = True
-    
 
     def draw_cell(self, window: pygame.Surface) -> None:
         """Draws a cell of the sudoku board onto the pygame window
-        
+
         Arguments:
             window: the pygame window
         """
-        #refer to correct cell by pixels pointing to top-left corner of cell
-        cell_x = CELL_SIDE_LENGTH * self.column 
-        cell_y = CELL_SIDE_LENGTH * self.row 
-       
-        #display non-zero cells
+        # refer to correct cell by pixels pointing to top-left corner of cell
+        cell_x = CELL_SIDE_LENGTH * self.column
+        cell_y = CELL_SIDE_LENGTH * self.row
+
+        # display non-zero cells
         if self.value:
             value_text_font = pygame.font.Font(FONT_FILE_PATH, CELL_FONT_SIZE)
-            value_text = value_text_font.render(str(self.value), True, CELL_FONT_COLOR)
+            value_text = value_text_font.render(
+                str(self.value), True, CELL_FONT_COLOR)
 
             value_text_width = value_text.get_width()
             value_text_height = value_text.get_height()
 
-            window.blit(value_text, (cell_x + CELL_SIDE_LENGTH // 2 - value_text_width // 2, cell_y + CELL_SIDE_LENGTH // 2 - value_text_height // 2))
-        
-        #display guess if one exists
+            window.blit(value_text, (cell_x + CELL_SIDE_LENGTH // 2 - value_text_width //
+                        2, cell_y + CELL_SIDE_LENGTH // 2 - value_text_height // 2))
+
+        # display guess if one exists
         if self.guess:
             guess_font = pygame.font.Font(FONT_FILE_PATH, GUESS_FONT_SIZE)
-            guess_text = guess_font.render(str(self.guess), True, GUESS_FONT_COLOR)
+            guess_text = guess_font.render(
+                str(self.guess), True, GUESS_FONT_COLOR)
 
-            window.blit(guess_text, (cell_x + GUESS_FONT_SIZE // 2, cell_y + GUESS_FONT_SIZE // 2))
+            window.blit(guess_text, (cell_x + GUESS_FONT_SIZE //
+                        2, cell_y + GUESS_FONT_SIZE // 2))
 
-        #if a cell is selected, add a border around it
+        # if a cell is selected, add a border around it
         if self.is_selected:
-            select_border = pygame.Rect(cell_x, cell_y, CELL_SIDE_LENGTH, CELL_SIDE_LENGTH)
-            pygame.draw.rect(window, SELECT_BORDER_COLOR, select_border, THICK_LINE_WIDTH)
+            select_border = pygame.Rect(
+                cell_x, cell_y, CELL_SIDE_LENGTH, CELL_SIDE_LENGTH)
+            pygame.draw.rect(window, SELECT_BORDER_COLOR,
+                             select_border, THICK_LINE_WIDTH)
 
 
 def main() -> None:
@@ -316,30 +319,30 @@ def main() -> None:
     The user can input guesses and play sudoku
     Additionally, the user can press the spacebar to solve the board using a backtracking algorithm
     """
-    #initializations
+    # initializations
     input_number = None
 
-    #create window 
+    # create window
     GAME_WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Sudoku")
 
-    #create board
+    # create board
     board = SudokuBoard(GAME_WINDOW)
 
-    #start loop
+    # start loop
     run = True
     while run:
         for event in pygame.event.get():
-            #end loop on close
+            # end loop on close
             if event.type == pygame.QUIT:
                 run = False
-            
-            #select cells on mouseclick
+
+            # select cells on mouseclick
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 board.select_cell(mouse_x, mouse_y)
-            
-            #take in inputs from keyboard
+
+            # take in inputs from keyboard
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1 or event.key == pygame.K_KP1:
                     input_number = 1
@@ -359,8 +362,8 @@ def main() -> None:
                     input_number = 8
                 if event.key == pygame.K_9 or event.key == pygame.K_KP9:
                     input_number = 9
-                
-                #autosolve
+
+                # autosolve
                 if event.key == pygame.K_SPACE:
                     if board.auto_solve():
                         board.freeze_board()
@@ -368,19 +371,19 @@ def main() -> None:
                     else:
                         board.freeze_board()
                         board.footer_message = "Unsolvable Board :("
-                
-                #for selected cells there are 3 actions
+
+                # for selected cells there are 3 actions
                 if board.currently_selected_cell is not None:
-                    #display temporary guess
+                    # display temporary guess
                     if input_number:
                         board.display_guess(input_number)
                         input_number = None
-                    
-                    #clear selected cells
+
+                    # clear selected cells
                     if (event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE or event.key == pygame.K_CLEAR):
                         board.clear_cell()
-                    
-                    #"lock in" temporary guess on selected cells
+
+                    # "lock in" temporary guess on selected cells
                     if (event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER):
                         board.lock_in_guess()
 
